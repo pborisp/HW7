404d5bf6c89712365998a59ce01ab6546e33d0cc
pborisp <popovbs@gmail.com>
2025-02-19 02:31:21 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        //Задача №1
+        int[] mas1 = new int[] {1, 2, 3};
+        double[] mas2 = {1.57, 7.654, 9.986};
+        int[] mas3 = {1, 2, 3, 4, 5};
 
     }
 }
