54dfa3634abf7d5272f683562192e3b6cbf81582
pborisp <popovbs@gmail.com>
2025-02-19 02:32:48 +0300

    Task 1,2

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,31 @@ public class Main {
         double[] mas2 = {1.57, 7.654, 9.986};
         int[] mas3 = {1, 2, 3, 4, 5};
 
+        // Задача №2
+        System.out.println("Задача №2:");
+        for (int i = 0; i < mas1.length; i++) {
+            if (i == mas1.length - 1) {
+                System.out.print(mas1[i]);
+                break;
+            }
+            System.out.print(mas1[i] + ", ");
+        }
+        System.out.println();
+        for (int i = 0; i < mas2.length; i++) {
+            if (i == mas2.length - 1) {
+                System.out.print(mas2[i]);
+                break;
+            }
+            System.out.print(mas2[i] + ", ");
+        }
+        System.out.println();
+        for (int i = 0; i < mas3.length; i++) {
+            if (i == mas3.length - 1) {
+                System.out.print(mas3[i]);
+                break;
+            }
+            System.out.print(mas3[i] + ", ");
+        }
+        System.out.println();
     }
 }
