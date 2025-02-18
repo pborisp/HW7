25f2205babf466ef5828de61e4c65e18f9051c3e
pborisp <popovbs@gmail.com>
2025-02-19 02:52:42 +0300

    Task 1,2,3,4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         //Задача №1
@@ -59,5 +61,20 @@ public class Main {
             System.out.print(mas3[i] + ", ");
         }
         System.out.println();
+        System.out.println();
+
+        // Задача №4
+        System.out.println("Задача №4:");
+        int[] arr = new int[12];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 10);
+        }
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                arr[i]++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
